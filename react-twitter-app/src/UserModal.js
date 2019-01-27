aee7b265f13b89936c37b772dab60d62239f2034
lealgamb <lealgamb@usc.edu>
2019-01-27 12:20:42 -0800

    bad button

--- react-twitter-app/src/UserModal.js
+++ react-twitter-app/src/UserModal.js
@@ -18,8 +18,15 @@ class UserModal extends React.Component {
                         <Modal.Title>{this.props.name}</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-                        {this.props.following.map(name => <p>{name}</p>)}
-                        {this.props.followers.map(name => <p>{name}</p>)}
+                        <div className="UserModal-following">
+                            <h4>Following</h4>
+                            {this.props.following.map(name => <p>{name}<Button>Remove</Button></p>)}
+                        </div>
+
+                        <div>
+                            <h4>Followers</h4>
+                            {this.props.followers.map(name => <p>{name}</p>)}
+                        </div>
                     </Modal.Body>
                     <Modal.Footer>
                         <Button onClick={this.props.handleClose}>Close</Button>
