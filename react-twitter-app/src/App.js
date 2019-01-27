ae601e8c33ed8ddc07c253feb02e26f84b7d5004
Jeffrey Hernandez <jeffredh@usc.edu>
2019-01-26 16:59:16 -0800

    added card and modal code

--- react-twitter-app/src/App.js
+++ react-twitter-app/src/App.js
@@ -1,28 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ItemCard from './ItemCard';
+import ItemModal from './ItemModal';
+class App extends React.Component
+{
+    constructor(props, context)
+    {
+        super(props, context);
+       this.state = {
+        items:[
+            {
+                itemName: "Pudge", 
+                itemDescription:"Best boy",
+                vendor:"The shelter",
+                locations:"Near you",
+                imgPath: "https://d3544la1u8djza.cloudfront.net/APHI/Blog/2017/January_01/training+a+pug+_+fawn+pug+with+a+hot++pink+harness+carrying+a+ball.jpg",
+                following:[
+                    
+                ]
+                
+            },
+            {
+                itemName: "Winston", 
+                itemDescription:"good boy",
+                vendor:"NOLA Shelter",
+                locations:"Near you",
+                imgPath: "https://irp-cdn.multiscreensite.com/94f3e251/dms3rep/multi/mobile/a-black-pug-adult-dog++%28275+x+275%29-min.jpg"
+            },
+            {
+                itemName: "Pooch", 
+                itemDescription:"Okay boy",
+                vendor:"Shelter",
+                locations:"Home",
+                imgPath:"https://thumbs.dreamstime.com/z/rat-terrier-chihuahua-mixed-breed-dog-86680681.jpg"
+            }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+        ]
+       };
+    }
+
+    displayItems = ()=>
+    {
+        return this.state.items.map(this.generateItem)
+    };
+
+    generateItem = (item)=>
+    {
+        return(<ItemCard
+            itemName={item.itemName}
+            vendor={item.vendor}
+            locations={item.locations}
+            imgPath={item.imgPath}
+            itemDescription={item.itemDescription}
+        />)
+    };
+
+    render()
+    {
+        return(
+            <div>
+                {this.displayItems()}
+            </div>
+        )
+    }
 }
-
 export default App;
---
+++ react-twitter-app/src/ItemCard.js
@@ -0,0 +1,53 @@
+import React from 'react';
+// import { className } from 'postcss-selector-parser';
+import './ItemCard.css';
+import ItemModal from './ItemModal';
+
+class ItemCard extends React.Component
+{
+
+    constructor(props, context)
+    {
+        super(props, context);
+        //this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            
+            isShowing: true
+        }
+    }
+    handleClick = (e) =>{
+        this.setState({
+            isShowing: !this.state.isShowing
+        });
+    }
+    
+    render(){
+        return(
+            <div className = "ItemCard-container">
+            <img class="ItemCard-img" src={this.props.imgPath}/>
+            {!this.state.isShowing && 
+                <ItemModal
+                    itemTitle={this.props.itemTitle}
+                    itemDescription={this.props.itemDescription}
+                    handleClose = {this.handleClick}
+                />
+            
+            }
+            <h4 className="ItemCard-title">
+                {this.props.vendor}
+            </h4>
+            <p style = {{textAlign:"center"}}>
+            </p>
+            <p style ={{textAlign:"center"}}>
+                <span style={{fontWeight:"bold"}}>
+                    {this.props.locations}
+                </span>
+            </p>
+            <button onClick={this.handleClick}>Click me</button>
+            </div>
+            
+        )
+    }
+}
+
+export default ItemCard;
